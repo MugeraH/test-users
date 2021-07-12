955ff27083a7284628fa39a4f92b6969cc0d7496
MugeraH <bwosihughes24@gmail.com>
2021-07-12 16:04:07 +0300

    add filter component to filter users list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="container">
-    <h2>Users</h2>
+    <div class="title">
+      <h2>Users</h2>
+      <FilterUsers />
+    </div>
+
     <div class="user-container">
       <User />
     </div>
 
-    <div class="loader" v-if="!users"></div>
+    <div class="loader" v-if="allUsers.length === 0"></div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import User from "../components/User.vue";
+import FilterUsers from "../components/FilterUsers.vue";
 
 export default {
   name: "Home",
+  computed: mapGetters(["allUsers"]),
   components: {
     User,
+    FilterUsers,
   },
 };
 </script>
@@ -29,5 +37,12 @@ h2 {
   justify-content: space-around;
   flex-wrap: wrap;
   gap: 2rem;
+}
+
+.title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
 }
 </style>
